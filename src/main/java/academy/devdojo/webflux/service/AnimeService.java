b8f6c61495786fe9092cd11c344f20eadaae9304
Jairo <jaironsousa@gmail.com>
2020-12-26 10:00:20 -0300

    :construction:  SwitchIfEmpty and ResponseStatusException
    Configuração no arquivo properties

--- src/main/java/academy/devdojo/webflux/service/AnimeService.java
+++ src/main/java/academy/devdojo/webflux/service/AnimeService.java
@@ -3,7 +3,9 @@ package academy.devdojo.webflux.service;
 import academy.devdojo.webflux.domain.Anime;
 import academy.devdojo.webflux.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -18,6 +20,12 @@ public class AnimeService {
     }
 
     public Mono<Anime> findbyId(int id){
-        return animeRepository.findById(id);
+        return animeRepository.findById(id)
+                .switchIfEmpty(monoResponseStatusNotFoundException())
+                .log();
+    }
+
+    public <T> Mono<T> monoResponseStatusNotFoundException() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime Not Found"));
     }
 }
