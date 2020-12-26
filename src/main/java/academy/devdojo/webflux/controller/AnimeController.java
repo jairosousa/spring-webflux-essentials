2fc2d5ca43f23faaa5b47b380850410d497d5b3b
Jairo <jaironsousa@gmail.com>
2020-12-26 09:49:28 -0300

    :construction: Ajuste R@DBC Spring Query DSL
    Configuração no arquivo properties

--- src/main/java/academy/devdojo/webflux/controller/AnimeController.java
+++ src/main/java/academy/devdojo/webflux/controller/AnimeController.java
@@ -1,13 +1,15 @@
 package academy.devdojo.webflux.controller;
 
 import academy.devdojo.webflux.domain.Anime;
-import academy.devdojo.webflux.repository.AnimeRepository;
+import academy.devdojo.webflux.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,11 +17,16 @@ import reactor.core.publisher.Flux;
 @Slf4j
 public class AnimeController {
 
-    private final AnimeRepository animeRepository;
+    private final AnimeService animeService;
 
     @GetMapping
     public Flux<Anime> listAll() {
         log.info("listando todos os registros ");
-        return animeRepository.findAll();
+        return animeService.findAll();
+    }
+
+    @GetMapping(path = "{id}")
+    public Mono<Anime> findById(@PathVariable int id) {
+        return animeService.findbyId(id);
     }
 }
