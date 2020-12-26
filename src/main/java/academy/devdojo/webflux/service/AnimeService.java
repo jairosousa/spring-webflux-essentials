466cfdcf8d4ffc2717ebf62db240d543450877f8
Jairo <jaironsousa@gmail.com>
2020-12-26 10:23:39 -0300

    :construction:  Global Exception Handler
    Melhoria na resposta do Json

--- src/main/java/academy/devdojo/webflux/service/AnimeService.java
+++ src/main/java/academy/devdojo/webflux/service/AnimeService.java
@@ -21,8 +21,7 @@ public class AnimeService {
 
     public Mono<Anime> findbyId(int id){
         return animeRepository.findById(id)
-                .switchIfEmpty(monoResponseStatusNotFoundException())
-                .log();
+                .switchIfEmpty(monoResponseStatusNotFoundException());
     }
 
     public <T> Mono<T> monoResponseStatusNotFoundException() {
